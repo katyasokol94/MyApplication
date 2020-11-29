5dab9977325a99acf37f057098b1031b2433684a
Katya Sokol <katya.sokol@gmail.com>
2020-11-29 20:37:23 +0300

    Implemented navigation between fragments.

--- app/src/main/java/com/sokolkatya/myapplication/ui/FragmentMoviesList.kt
+++ app/src/main/java/com/sokolkatya/myapplication/ui/FragmentMoviesList.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.sokolkatya.myapplication.R
 
 class FragmentMoviesList : Fragment() {
+
+    private var listener: MovieClickListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +18,22 @@ class FragmentMoviesList : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val clMovie: ConstraintLayout = view.findViewById(R.id.cl_movie_item)
+        clMovie.setOnClickListener {
+            listener?.onClick()
+        }
+    }
+
+    fun setClickListener(l: MovieClickListener?) {
+        listener = l
+    }
+
+    interface MovieClickListener {
+        fun onClick()
     }
 }
--- app/src/main/java/com/sokolkatya/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/sokolkatya/myapplication/ui/MainActivity.kt
@@ -2,27 +2,55 @@ package com.sokolkatya.myapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.sokolkatya.myapplication.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesList.MovieClickListener,
+    FragmentMoviesDetails.MovieDetailsClickListener {
 
-    private var movieListFragment: Fragment? = null
-    private var movieDetailsFragment: Fragment? = null
+    private var movieListFragment =
+            FragmentMoviesList().apply { setClickListener(this@MainActivity) }
+    private var movieDetailsFragment =
+            FragmentMoviesDetails().apply { setClickListener(this@MainActivity) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (movieListFragment == null) {
-            movieListFragment = FragmentMoviesList()
-            if (supportFragmentManager.findFragmentByTag("movieListFragment") == null) {
-                supportFragmentManager.beginTransaction()
-                        .apply {
-                            add(R.id.flContainer, movieListFragment!!, "movieListFragment")
-                            commit()
-                        }
-            }
+        supportFragmentManager.beginTransaction()
+                .apply {
+                    add(R.id.flContainer, movieListFragment)
+                    commit()
+                }
+    }
+
+    override fun onClick() {
+        supportFragmentManager.beginTransaction()
+                .apply {
+                    add(R.id.flContainer, movieDetailsFragment)
+                    commit()
+                }
+    }
+
+    override fun onBackClick() {
+        removeLastFragment()
+    }
+
+    private fun removeLastFragment() {
+        if (supportFragmentManager.fragments.size > 1) {
+            val lastFragment = supportFragmentManager.fragments.last()
+            supportFragmentManager.beginTransaction()
+                    .apply {
+                        remove(lastFragment)
+                        commit()
+                    }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.fragments.size > 1) {
+            removeLastFragment()
+        } else {
+            super.onBackPressed()
         }
     }
 }
