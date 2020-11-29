9abd9965f1062277c5b5d8b5dabbd1fef405413c
Katya Sokol <katya.sokol@gmail.com>
2020-11-29 19:42:00 +0300

    Added UI for 'Movie List' screen.

--- app/src/main/java/com/sokolkatya/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/sokolkatya/myapplication/ui/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.sokolkatya.myapplication.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.sokolkatya.myapplication.R
 
 class MainActivity : AppCompatActivity() {
 
+    private var movieListFragment: Fragment? = null
+    private var movieDetailsFragment: Fragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        val tvMoveToNextScreen = findViewById<TextView>(R.id.tv_move_to_next_screen)
-        tvMoveToNextScreen.setOnClickListener {
-            val intent: Intent = Intent(this, MovieDetailsActivity::class.java)
-            startActivity(intent)
+        if (movieListFragment == null) {
+            movieListFragment = FragmentMoviesList()
+            if (supportFragmentManager.findFragmentByTag("movieListFragment") == null) {
+                supportFragmentManager.beginTransaction()
+                        .apply {
+                            add(R.id.flContainer, movieListFragment!!, "movieListFragment")
+                            commit()
+                        }
+            }
         }
     }
 }
